c8e0a03e1b878cd5afee13025ef5cc6e073dc7eb
enzoflores <enzoflores15k@gmail.com>
2023-11-13 17:10:04 -0300

    3

--- productManager.js
+++ productManager.js
@@ -1,61 +1,53 @@
-
 class ProductManager {
 
     constructor() {
-        this.product = []
+        this.product = [];
         this.contador = 1;
     }
     getProducts() {
         console.log(this.product);
     }
 
+
+    getProductsById(id) {
+        let productID = this.product.find((producto) => producto.id == id)
+        console.log(productID);
+        return productID;
+    }
+
     addProduct(product) {
-        
 
         let productoCode = this.product.find((producto) => producto.code == product.code)
 
         if (productoCode) {
             console.log("Code repetido");
+            return;
         } else {
-            product.id = this.contador
-            this.contador++
-            this.product.push(product)
+            product.id = this.contador;
+            this.contador++;
+            this.product.push(product);
         }
     }
 
-
-    getProductById(id) {
-        let productID = this.product.find((producto) => producto.id == id)
-        console.log(productID);
-        return productID;
-    }
 }
 
 class Product {
-    constructor(
-        title,
-        description,
-        price,
-        thumbnail,
-        code,
-        stock
-    ) {
-        this.title = title;
-        this.description = description;
-        this.price = price;
-        this.thumbnail = thumbnail;
-        this.code = code;
-        this.stock = stock
+    constructor(title, description, price, thumbnail, code, stock) {
+        this.title = title || " ";
+        this.description = description || " ";
+        this.price = price || 0;
+        this.thumbnail = thumbnail || " ";
+        this.code = code || 0;
+        this.stock = stock || 0;
     }
 }
 
-let producto1 = new Product ("producto1", "Silla", 400, "dad", 1, 10);
-
+let producto1 = new Product("producto1", "Silla", 400);
 
 // Pruebas
 
-const productManager = new ProductManager
+const productManager = new ProductManager();
 
 productManager.addProduct(producto1);
 
-productManager.getProducts()
+productManager.getProducts();
